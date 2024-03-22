76f3407835019d4edde49c84f0eb42ca907053f5
Emir Smagulov <tylerdurden28062@gmail.com>
2024-03-22 22:09:35 +0600

    fix: everything

--- src/modules/contacts/Contacts.jsx
+++ src/modules/contacts/Contacts.jsx
@@ -1,11 +1,11 @@
 import './contacts.css';
-import map from '../../assets/img/карта.png'
+
 function Contacts() {
 
     return (
         <>
             <div className="contacts">
-                <img src={map} alt="" />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d186084.69680889035!2d76.7877092644574!3d43.21843652955674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836e7d16c5cbab%3A0x3d44668fad986d76!2z0JDQu9C80LDRgtGL!5e0!3m2!1sru!2skz!4v1711122681698!5m2!1sru!2skz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <h3>Contacts</h3>
                 <ul>
                     <li>Phone : 88005553535</li>
